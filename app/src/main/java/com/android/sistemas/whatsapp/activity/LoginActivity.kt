c1020dd0a3cb5276717744713d6d42ca3b7d8de4
Jardier <jardier@hotmail.com>
2021-02-21 08:38:19 -0300

    Criando a interface de login

---
+++ app/src/main/java/com/android/sistemas/whatsapp/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.android.sistemas.whatsapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.sistemas.whatsapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
