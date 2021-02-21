aa51a880430bd774cc453fdf45a8374e72481124
Jardier <jardier@hotmail.com>
2021-02-21 09:28:16 -0300

    Criando a interface Cadastro

---
+++ app/src/main/java/com/android/sistemas/whatsapp/activity/CadastroActivity.kt
@@ -0,0 +1,12 @@
+package com.android.sistemas.whatsapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.sistemas.whatsapp.R
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
--- app/src/main/java/com/android/sistemas/whatsapp/activity/LoginActivity.kt
+++ app/src/main/java/com/android/sistemas/whatsapp/activity/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.android.sistemas.whatsapp.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.android.sistemas.whatsapp.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+
+
+
+
+    fun exibirTelaCadastro(view : View) {
+        val intent = Intent(this, CadastroActivity::class.java);
+        startActivity(intent);
     }
 }
