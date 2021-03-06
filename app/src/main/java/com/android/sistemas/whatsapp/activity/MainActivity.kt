83be0c1b67b7311c2f7b638dd38b9abb84fe012a
Jardier <jardier@hotmail.com>
2021-03-06 15:29:31 -0300

    Criando e configurando o menu na toolbar

--- app/src/main/java/com/android/sistemas/whatsapp/activity/MainActivity.kt
+++ app/src/main/java/com/android/sistemas/whatsapp/activity/MainActivity.kt
@@ -2,12 +2,19 @@ package com.android.sistemas.whatsapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.android.sistemas.whatsapp.R
+import com.android.sistemas.whatsapp.config.FireBaseConfig
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toolbarPrincipal: Toolbar;
+    private lateinit var autenticacao : FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +24,41 @@ class MainActivity : AppCompatActivity() {
         toolbarPrincipal.title = "WhatsApp";
         setSupportActionBar(toolbarPrincipal);
 
+        autenticacao = FireBaseConfig.autenticacao;
+    }
+
+    //Inserindo o menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Item do menu selecionado
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.menuPesquisa -> {
+                Toast.makeText(this, "Menu Pesquisa", Toast.LENGTH_LONG).show();
+            }
+            R.id.menuConfiguracoes -> {
+                Toast.makeText(this, "Menu Configurações", Toast.LENGTH_LONG).show();
+
+            }
+            R.id.menuSair -> {
+                deslogarUsuario();
+                finish();
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deslogarUsuario() {
+        try {
+            autenticacao.signOut();
+
+        } catch (e: Exception) {
+            e.printStackTrace();
+        }
     }
 }
