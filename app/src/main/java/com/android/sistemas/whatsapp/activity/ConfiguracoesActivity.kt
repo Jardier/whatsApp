c6c0242119a8712ffd711b0f56eb00465524e708
Jardier <jardier@hotmail.com>
2021-03-13 15:14:52 -0300

    Incluindo recursos de Câmera e Imagem

--- app/src/main/java/com/android/sistemas/whatsapp/activity/ConfiguracoesActivity.kt
+++ app/src/main/java/com/android/sistemas/whatsapp/activity/ConfiguracoesActivity.kt
@@ -2,9 +2,13 @@ package com.android.sistemas.whatsapp.activity
 
 import android.Manifest
 import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import com.android.sistemas.whatsapp.R
@@ -14,9 +18,13 @@ class ConfiguracoesActivity : AppCompatActivity() {
 
     private lateinit var toolbarPrincipal: Toolbar;
     private lateinit var permissoesNecessarias : ArrayList<String>
+    private lateinit var botaoCamera : ImageButton;
+    private lateinit var botaoGaleria : ImageButton;
 
     companion object{
          const val REQUEST_CODE : Int = 1;
+         const val REQUEST_CODE_CAMERA : Int = 100;
+         const val REQUEST_CODE_GALERIA : Int = 200;
      }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,12 +36,24 @@ class ConfiguracoesActivity : AppCompatActivity() {
         setSupportActionBar(toolbarPrincipal);
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
+        botaoGaleria = findViewById(R.id.btnGaleria);
+        botaoCamera = findViewById(R.id.btnCamera);
+
 
         //Permissões Necessárias
         permissoesNecessarias = arrayListOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA);
 
         //Validar Permissões
         Permissao.validarPermissoes(permissoesNecessarias, this, REQUEST_CODE);
+
+        //Ação botao camera
+        botaoCamera.setOnClickListener(View.OnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            if(intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, REQUEST_CODE_CAMERA);
+            }
+        })
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
