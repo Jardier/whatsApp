8993ebeb0b3ab401c6a18fb72c0cbd0c64dbb6d6
Jardier <jardier@hotmail.com>
2021-03-13 14:19:49 -0300

    Sistema de permissões Android Marshmallow

--- app/src/main/java/com/android/sistemas/whatsapp/activity/ConfiguracoesActivity.kt
+++ app/src/main/java/com/android/sistemas/whatsapp/activity/ConfiguracoesActivity.kt
@@ -1,13 +1,23 @@
 package com.android.sistemas.whatsapp.activity
 
+import android.Manifest
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import com.android.sistemas.whatsapp.R
+import com.android.sistemas.whatsapp.helper.Permissao
 
 class ConfiguracoesActivity : AppCompatActivity() {
 
     private lateinit var toolbarPrincipal: Toolbar;
+    private lateinit var permissoesNecessarias : ArrayList<String>
+
+    companion object{
+         const val REQUEST_CODE : Int = 1;
+     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +28,35 @@ class ConfiguracoesActivity : AppCompatActivity() {
         setSupportActionBar(toolbarPrincipal);
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
+
+        //Permissões Necessárias
+        permissoesNecessarias = arrayListOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA);
+
+        //Validar Permissões
+        Permissao.validarPermissoes(permissoesNecessarias, this, REQUEST_CODE);
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        grantResults.forEach { grantResult : Int ->
+            if(grantResult == PackageManager.PERMISSION_DENIED) {
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private fun alertaValidacaoPermissao() {
+        val builder = AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o App é necessário aceitar as permissões");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton(getString(R.string.sim), DialogInterface.OnClickListener{dialog, which ->
+              finish();
+        });
+        val dialog : AlertDialog = builder.create();
+        dialog.show();
 
     }
 }
---
+++ app/src/main/java/com/android/sistemas/whatsapp/helper/Permissao.kt
@@ -0,0 +1,41 @@
+package com.android.sistemas.whatsapp.helper
+
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.android.sistemas.whatsapp.config.FireBaseConfig
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+class Permissao {
+
+    companion object{
+        private const val SDK_MARSHMALLOW = 21;
+
+        fun validarPermissoes(permissoes : ArrayList<String>, activity: Activity, requestCode : Int) : Boolean{
+
+            //Verifica a versão do SDK
+            if(Build.VERSION.SDK_INT >= SDK_MARSHMALLOW) {
+                val listaPermissoes : ArrayList<String> = ArrayList();
+
+                permissoes.forEach {permissao : String ->
+                    val temPermissao = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
+                    if(!temPermissao) {
+                        listaPermissoes.add(permissao);
+                    }
+                }
+                //Caso a lista esteja vazia, não é necessário solicitar as permissões
+                if(listaPermissoes.isEmpty()) return true;
+
+                //Solicitar permissões
+                ActivityCompat.requestPermissions(activity, listaPermissoes.toTypedArray(), requestCode);
+            }
+
+            return true;
+        }
+    }
+}
